241305c4a3902831a04ca23cde1453a8969366a3
Manish <tamangmanish3456@gmail.com>
2024-02-01 13:45:17 +0545

    moved root page to dashboard RG

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,12 +1,10 @@
-import { Button } from "@/components/ui/button";
+
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>
-    Hello World
-
-      </Button>
+      <UserButton afterSignOutUrl="/"/>
     </main>
   );
 }
