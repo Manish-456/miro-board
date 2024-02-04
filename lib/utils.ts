8dcaf7e4e537771c014248ce68b434cf6e10021f
Manish <tamangmanish3456@gmail.com>
2024-02-04 19:21:29 +0545

    add: participants

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const COLORS = [
+  "#DC2626", 
+  "#D97706", 
+  "#059669", 
+  "#7C3AED", 
+  "#DB2777"
+];
+
+export function connectionIdToColor(connectionId: number){
+ return COLORS[connectionId % COLORS.length]
+}
